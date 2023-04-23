9eeb5ef15e4b2c6ab32a8343b1b45ebe4f629198
RifatWaheed <rifatwaheed@gmail.com>
2023-04-23 22:32:58 +0600

    ternary operation practice

--- src/App.js
+++ src/App.js
@@ -2,34 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const planets = [
+    {name : "Mars" , isGasPlanet: false},
+    {name : "Earth" , isGasPlanet : true},
+    {name : "Jupiter", isGasPlanet : true },
+    {name : "Venus" , isGasPlanet : false}
+  ];
+
+
   return (
     <div className="App">
-      <Job salary={2000} position="Senior Sde" company="Amazon"/>
-      <Job salary={6000} position="jr sde" company="Google"/>
-      <Job salary={4500} position="Java dev" company="Facebook"/>
+      {planets.map((planet,key)=> {
+        return <Planet name={planet.name} isGasPlanet={planet.isGasPlanet}/>
+      }
 
+      )}
+    
 
     </div>
   );
 }
 
 
-
-const Job = (props)=>{
-  return(
-    <div>
-      <h2>{props.salary}</h2>
-      <h2>{props.position}</h2>
-      <h2>{props.comp}</h2>
-    </div>
-
-
-
-  )
-
-
+const Planet = (props) => {
+  return props.isGasPlanet && <h1>{props.name}</h1>
 
 
 }
+  
+    
+
+  
+
+
+
+
 
 export default App;
